26c09de8c02db1dbaf3db58797923fe465648694
Anton Kozhemyakin <hoppvo2020@mail.ru>
2024-10-03 13:49:28 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,17 @@
-n = int(input("Введите число от 3 до 20"))
-result = []
+import random
+list_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+n = random.choice(list_)
+result_base = []
 for j in range(1, 21):
     for k in range(1, 21):
         _ = j + k
         if n == _ and j < k:
-            result.append([j, k])
+            result_base.append([j, k])
         elif n % _ == 0 and j < k:
-            result.append([j, k])
-print(f"{n} - {result}")
+            result_base.append([j, k])
+print(n)
+result = ''
+for pair in result_base:
+    result += f'{pair[0]} {pair[1]} '
+result = result.replace(" ", "")
+print(result.strip())
